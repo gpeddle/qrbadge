e8ba4efa2e52aa1a235b3b72c65e8f409f6e0dfb
Greg Peddle <gpeddle@gmail.com>
2021-09-14 12:07:53 -0400

    add example and command line args

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ qrbadge -c config_file -i input_file -o output_dir
 - `output_dir` - writeable folder for output image files
 
 
+## Config files
+
+Example:
+
+```xml
+<?xml version ='1.0'?>
+<spec xmlns="spec">
+    <fileName>{UniqueId}-{LastName}.png</fileName>
+    <document orientation="portrait" height="350" width="350" margin="25" units="px">
+        <code width="325" height="325">{UniqueId}^{LastName}^{FirstName}</code>
+        <text font="Calibri" fontSize="32">{FirstName}</text>
+        <text font="Calibri" fontSize="22">{LastName}</text>
+        <text font="Calibri" fontSize="14">{Company}</text>
+    </document>
+</spec>
+```
+
+## Running the Example
+
+`cargo run -- --input example/data/data.txt  --output example/out --spec example/spec/badge.xml`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+extern crate minidom;
+use structopt::StructOpt;
+
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The path to the config file
+    #[structopt( long = "spec", parse(from_os_str))]
+    spec_file:  std::path::PathBuf,
+
+    /// The path to the file of data
+    #[structopt( long = "input", parse(from_os_str))]
+    input_file: std::path::PathBuf,
+
+    /// The path to output files
+    #[structopt( long = "output", parse(from_os_str))]
+    output_dir: std::path::PathBuf,
 }
+
+
+const _QRSPEC_NS: &'static str = "qrspec";
+
+
+#[derive(Debug)]
+pub struct QrSpec {
+
+    file_pattern: String,
+
+    title: String,
+    body: String,
+}
+
+#[derive(Debug)]
+pub enum Unit {
+    Inches,
+    Millimeters
+}
+
+#[derive(Debug)]
+pub struct QrDocument {
+
+    units: Unit,
+    width: f32,
+    height: f32,
+    margin: f32
+}
+
+
+
+
+
+fn main() {
+
+    
+    let args = Cli::from_args();
+
+ 
+
+    let contents = fs::read_to_string(args.spec_file)
+        .expect("Error while reading the spec file");
+    
+
+    let root: minidom::Element = contents.parse().unwrap();
+    println!("{:#?}", root);
+    //let input_file = std::env::args().nth(2).expect("no input data file provided");
+    //let output_dir = std::env::args().nth(3).expect("no output dir provided");
+
+    //let qq: QrDocument = new_qr_document();
+
+    let _data = fs::read_to_string(args.input_file)
+    .expect("Error while reading the input file");
+
+    let _xx = args.output_dir;
+
+    
+}   
